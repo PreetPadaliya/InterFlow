f97680ea8e03ba1e97a3636e9a01a38ddd417b91
PreetPadaliya <preetpadaliya2805@gmail.com>
2026-06-04 22:37:12 +0530

    added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-black h-screen text-white flex items-center justify-center text-4xl">
+      InterviewFlow
+    </div>
+  )
+}
+
+export default App
